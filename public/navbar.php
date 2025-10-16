eb719bd6ec0972a6760fa4cfdd6ca5f03f5b0541
João <jvcolombo.06@gmail.com>
2025-10-16 18:38:49 -0300

    fix: to homepage

--- public/navbar.php
+++ public/navbar.php
@@ -19,7 +19,7 @@ if ($connect) {
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="../public/index.php">UniMusic</a>
+        <a class="navbar-brand fw-bold" href="../index.php">UniMusic</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
